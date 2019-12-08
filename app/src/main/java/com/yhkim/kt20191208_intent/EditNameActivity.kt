ed7ae59e3f4c5d99f8885fa1265bcd5db3815b60
kvoyant <kvoyant@gmail.com>
2019-12-08 10:12:22 +0900

    입력화면 그리기

---
+++ app/src/main/java/com/yhkim/kt20191208_intent/EditNameActivity.kt
@@ -0,0 +1,12 @@
+package com.yhkim.kt20191208_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_name)
+    }
+}
